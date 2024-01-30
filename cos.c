0f57786246ec71ed0c0d60e5ac6551eeb3b33983
brunood1 <97195236+brunood1@users.noreply.github.com>
2024-01-30 00:10:30 -0300

    Update and rename ll.c to cos.c

--- cos.c
+++ cos.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
-// gcc lista.c -o lista
-// ./lista > NOME.off
+// gcc cos.c -o cos
+// ./cos > cos.off
 
 // ABRIR COM MESHLAB
 
 int main(){
     printf("OFF\n");
-    printf("441 400 420\n");
+    printf("441 400 840\n");
     for (int y = -10; y <= 10; y++){
         for (int x = -10; x <= 10; x++){
-            printf("%d %d %d\n", x, y, 3*x + y); // TROCAR FUNÇÃO AQUI
+            printf("%d %d %d\n", x, y, cos(sqrt(x*x + y*y))); // TROCAR FUNÇÃO AQUI
         }
     }
-    for (int j = 0; j < 110; j+=21){
+    for (int j = 0; j < 420; j+=21){
         for (int i = 0; i < 20; i++){
             printf("4 %d %d %d %d\n", i+j, i+21+j, i+22+j, i+1+j);
         }
